76380d6b7f4abb14fba3c4e49ceb9d5217221e5e
Muhammad Osama <madebyosama@gmail.com>
2024-11-24 22:37:48 +0500

    Refactor: Faster load

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,40 @@
 'use client';
 
-interface Icon {
-  title: string;
-  code: string;
-  type: string;
-}
+// interface Icon {
+//   title: string;
+//   code: string;
+//   type: string;
+// }
 
-import { useEffect, useState } from 'react';
-import Loading from './components/Loading/Loading';
+// import { useEffect, useState } from 'react';
 import { downloadSvg } from './utils/download';
+import huge from './data/huge';
 
 export default function Home() {
-  const [icons, setIcons] = useState<Icon[]>([]);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+  // const [icons, setIcons] = useState<Icon[]>(huge);
+  // const [loading, setLoading] = useState<boolean>(true);
+  // const [error, setError] = useState<string | null>(null);
 
-  useEffect(() => {
-    const fetchIcons = async () => {
-      try {
-        const response = await fetch(
-          'https://opensheet.elk.sh/1gJdgS9vawxEVNmISvPsaNKTkChTGEQwwOMIQiyH_NdM/huge'
-        );
-        if (!response.ok) {
-          throw new Error('Failed to fetch icons');
-        }
-        const data: Icon[] = await response.json();
-        setIcons(data);
-      } catch (err) {
-        setError(err instanceof Error ? err.message : 'An error occurred');
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchIcons = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         'https://opensheet.elk.sh/1gJdgS9vawxEVNmISvPsaNKTkChTGEQwwOMIQiyH_NdM/huge'
+  //       );
+  //       if (!response.ok) {
+  //         throw new Error('Failed to fetch icons');
+  //       }
+  //       const data: Icon[] = await response.json();
+  //       setIcons(data);
+  //     } catch (err) {
+  //       setError(err instanceof Error ? err.message : 'An error occurred');
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    fetchIcons();
-  }, []);
+  //   fetchIcons();
+  // }, []);
 
   const handleCopy = async (code: string) => {
     await navigator.clipboard.writeText(code);
@@ -44,18 +44,18 @@ export default function Home() {
     downloadSvg(code, title);
   };
 
-  if (loading) {
-    return <Loading />;
-  }
+  // if (loading) {
+  //   return <div></div>;
+  // }
 
-  if (error) {
-    return <div className='error'>Error: {error}</div>;
-  }
+  // if (error) {
+  //   return <div className='error'>Error: {error}</div>;
+  // }
 
   return (
     <main className='container'>
       <div className='icons'>
-        {icons.map((icon, index) => (
+        {huge.map((icon, index) => (
           <div
             key={`${icon.title}-${index}`}
             className='icon'
